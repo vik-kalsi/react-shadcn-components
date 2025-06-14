8ae63654a6c2bcce528fb76a548d52c110ff849c
vik-kalsi <kalsivik@googlemail.com>
2025-06-14 14:24:33 +0100

    Modify vite config to deploy to GitHub page

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react-shadcn-components/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
